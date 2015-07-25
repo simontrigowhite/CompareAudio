86d4f27b8974450457ff6957e062426ceba58633
Simon White <simon@trigowhite.com>
2015-07-25 16:02:44 +0100

    first commit - NAudio works

---
+++ CompareAudio/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NAudio.Wave;
+
+namespace CompareAudio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //Declarations required for audio out and the MP3 stream
+            IWavePlayer waveOutDevice;
+            AudioFileReader audioFileReader;
+
+            waveOutDevice = new WaveOut();
+
+            try
+            {
+
+                audioFileReader = new AudioFileReader("c:\\Users\\Simon\\Desktop\\3 2 5.5.wav");
+
+                waveOutDevice.Init(audioFileReader);
+                waveOutDevice.Play();
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("error " + ee.Message);
+            }
+
+
+        }
+    }
+}
